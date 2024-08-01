77cc9234038dd5b1d70ba226724502a3444a9574
CV145 <cvaleriano564@gmail.com>
2024-07-31 19:45:00 -0500

    merge?

--- TestServer/server.cpp
+++ TestServer/server.cpp
@@ -1,207 +1,226 @@
-#include <iostream>
-#include <cstring>
 #include <sys/socket.h>
+#include <iostream>
 #include <netinet/in.h>
+#include <cstring>
 #include <unistd.h>
-#include <ctime>
-#include <arpa/inet.h>
+#include <cstdio>
 #include <thread>
+#include <ctime>
+#include <atomic>
 
-// Using individual declarations to avoid std:: prefixes
 using namespace std;
 
 struct MessageHeader
 {
-    uint8_t header_length;
-    uint8_t message_type;
-    uint32_t timestamp;
+    uint8_t headerLen;
+    uint8_t messageType;
+    uint32_t timeStamp;
     uint16_t sender_id;
     uint16_t receiver_id;
     uint32_t message_id;
-    uint16_t payload_length;
+    uint16_t payloadLen;
+    uint16_t checksum;
 };
 
-// Function to send a message (MessageHeader)
-void sendMessage(int sock, uint8_t message_type, const string &message)
+atomic<bool> exitThread{false};
+
+uint16_t checkSum_Gen(const uint8_t *data, size_t len)
 {
-    static int message_counter = 0; // Static counter for unique message IDs
-
-    MessageHeader header;
-    header.header_length = sizeof(MessageHeader);
-    header.message_type = message_type; // CHAT
-
-    // Set the current time as timestamp
-    header.timestamp = static_cast<uint32_t>(time(nullptr));
-    header.sender_id = 1;                  // Server ID
-    header.receiver_id = 2;                // Client ID
-    header.message_id = ++message_counter; // Generate unique message ID
-    header.payload_length = message.size();
-
-    // Combining MessageHeader and payload into single buffer:
-    // Calculate the total size of the message
-    // size_t represents the size of an object in bytes
-    size_t total_size = sizeof(header) + message.size();
-
-    // Allocate memory for the combined buffer
-    char *buffer = new char[total_size];
-
-    // Copy the header to the buffer
-    memcpy(buffer, &header, sizeof(header));
-
-    // Copy the message payload to the buffer after the header
-    memcpy(buffer + sizeof(header), message.c_str(), message.size());
-
-    // Send the combined buffer over the socket
-    // Send all bytes of buffer through socket
-    send(sock, buffer, total_size, 0);
-
-    // Free the allocated memory
-    delete[] buffer;
+    uint32_t sum = 0;
+    for (int i = 0; i < len; i++)
+        sum += data[i];
+    return (uint16_t)sum;
 }
 
-// Function to handle client responses
-void handleClientResponse(int client_fd)
+bool checkSum_Check(uint16_t checkSum, size_t len)
 {
-    MessageHeader header;
-    while (true)
-    {
-        ssize_t bytesRead = read(client_fd, &header, sizeof(header));
-        if (bytesRead <= 0)
-        {
-            if (bytesRead == 0)
-            {
-                cout << "Client disconnected" << endl;
-            }
-            else
-            {
-                perror("Read error");
-            }
-            break;
-        }
-
-        switch (header.message_type)
-        {
-        case 1: // ON_REQ
-            cout << "Received online status request" << endl;
-            // Send online status response
-            sendMessage(client_fd, 2, "Server is online");
-            break;
-        case 3: // CHAT
-        {
-            char *payload = new char[header.payload_length];
-            ssize_t payloadBytesRead = read(client_fd, payload, header.payload_length);
-            if (payloadBytesRead != header.payload_length)
-            {
-                cerr << "Payload read error: Expected " << header.payload_length << " bytes, but got " << payloadBytesRead << " bytes." << endl;
-                delete[] payload;
-                break;
-            }
-            cout << "Client Message: " << string(payload, header.payload_length) << endl;
-            delete[] payload;
-
-            // Send ACK
-            MessageHeader ack;
-            ack.header_length = sizeof(MessageHeader);
-            ack.message_type = 4; // ACK
-            ack.timestamp = static_cast<uint32_t>(time(nullptr));
-            ack.sender_id = header.receiver_id;
-            ack.receiver_id = header.sender_id;
-            ack.message_id = header.message_id;
-            ack.payload_length = 0;
-            if (send(client_fd, &ack, sizeof(ack), 0) < 0)
-            {
-                perror("Send error");
-                break;
-            }
-            break;
-        }
-        default:
-            cerr << "Unknown message type received" << endl;
-            break;
-        }
-    }
+    return true;
 }
 
-// Function to handle sending messages from the server to the client
-void sendMessageToClient(int client_fd)
+void socket_Send(int sockfd, const u_int8_t msgType, const string &message)
 {
-    string message;
+    MessageHeader myHeader;
+    myHeader.headerLen = sizeof(MessageHeader);
+    myHeader.messageType = msgType;
+    myHeader.timeStamp = (uint32_t)time(nullptr);
+    myHeader.sender_id = 2;      // sample id
+    myHeader.receiver_id = 1;    // sample id
+    myHeader.message_id = 12345; // sample id
+    myHeader.payloadLen = message.size();
+    myHeader.checksum = 0; // calculate
+
+    size_t packetSize = myHeader.headerLen + myHeader.payloadLen;
+    uint8_t *buff = new uint8_t[packetSize];
+    memcpy(buff, &myHeader, sizeof(myHeader));
+    memcpy(buff + sizeof(myHeader), message.c_str(), message.size());
+    myHeader.checksum = checkSum_Gen(buff, packetSize);
+    memcpy(buff, &myHeader, sizeof(myHeader));
+
+    send(sockfd, buff, packetSize, 0); // Ensure sending the entire packet
+    delete[] buff;
+}
+
+void socket_Receive(int sockfd)
+{
+    MessageHeader *myHeader;
+    char buff[1024];
     while (true)
     {
-        cout << "Enter message: ";
-        getline(cin, message);
-        if (message == "quit")
+        cout << "Waiting for Msg\n";
+        while (recv(sockfd, buff, sizeof(buff), MSG_DONTWAIT | MSG_PEEK) == -1)
         {
-            break;
+            if (exitThread.load())
+                return;
         }
-        sendMessage(client_fd, 3, message); // Use message type 3 for chat messages
+        ssize_t inBytes = recv(sockfd, buff, sizeof(buff), 0);
+        if (inBytes > 0)
+        {
+            myHeader = reinterpret_cast<MessageHeader *>(buff);
+            cout << "Msg received\n";
+            time_t timeRaw = myHeader->timeStamp;
+            struct tm *timeInfo = localtime(&timeRaw);
+            char timeString[20];
+            strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", timeInfo);
+
+            cout << "Msg timestamp: " << timeString << "\n";
+            cout << "Msg type: " << (int)myHeader->messageType << ", Payload Length: " << myHeader->payloadLen << " bytes\n";
+
+            if (myHeader->payloadLen > sizeof(buff) - sizeof(MessageHeader))
+            {
+                cerr << "Message Length exceeds Buffer, dumping Message and sending Error\n";
+                socket_Send(sockfd, 6, to_string(myHeader->message_id));
+                continue;
+            }
+            else if (!checkSum_Check(myHeader->checksum, myHeader->payloadLen))
+            {
+                cerr << "Invalid checksum, sending Error Message\n";
+                socket_Send(sockfd, 6, to_string(myHeader->message_id));
+                continue;
+            }
+
+            string message(buff + sizeof(MessageHeader), myHeader->payloadLen);
+            switch ((int)myHeader->messageType)
+            {
+            case 1:
+                cout << "Status Request received from: " << (int)myHeader->sender_id << ", sending response\n";
+                socket_Send(sockfd, 2, to_string(myHeader->message_id));
+                break;
+            case 2:
+                cout << "Status Response received, " << (int)myHeader->sender_id << " is online\n";
+                break;
+            case 3:
+                cout << "Chat Message received: " << message << "\n";
+                socket_Send(sockfd, 4, to_string(myHeader->message_id));
+                break;
+            case 4:
+                cout << "Received ACK for Message " << message << "\n";
+                break;
+            case 5:
+                cout << "Received NACK for Message " << message << "\n";
+                break;
+            case 6:
+                cout << "Received Error for Message " << message << "\n";
+                break;
+            default:
+                cout << "Unknown message type: " << (int)myHeader->messageType << " received, sending Error Message\n";
+                socket_Send(sockfd, 6, to_string(myHeader->message_id));
+                break;
+            }
+        }
+        else if (inBytes == -1)
+        {
+            cerr << strerror(errno) << "\n";
+            close(sockfd);
+            return;
+        }
     }
 }
 
 int main()
 {
-    int server_fd, client_fd;
-    struct sockaddr_in address;
-    struct sockaddr_in client_address;
-    int opt = 1;
-    socklen_t addrlen = sizeof(address);
-    socklen_t client_addrlen = sizeof(client_address);
+    struct sockaddr_in saddr, caddr;
+    int sockfd = -1, isock = -1;
+    unsigned int clen;
+    unsigned short port = 8080;
+    bool isError;
+    int loops = 0;
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    do
     {
-        perror("Socket creation error");
-        return -1;
-    }
+        isError = false;
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-    {
-        perror("Setsockopt error");
-        return -1;
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-    {
-        perror("Bind failed");
-        return -1;
-    }
-
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("Listen failed");
-        return -1;
-    }
-
-    cout << "Server listening on port 8080" << endl;
-
-    while (true)
-    {
-        if ((client_fd = accept(server_fd, (struct sockaddr *)&client_address, &client_addrlen)) < 0)
+        if (loops > 1)
         {
-            perror("Accept failed");
-            continue;
+            cerr << "Failed to establish connection after 3 attempts, exiting program\n";
+            return EXIT_FAILURE;
         }
 
-        // Get and display client information
-        char client_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &client_address.sin_addr, client_ip, INET_ADDRSTRLEN);
-        cout << "Connection accepted from " << client_ip << ":" << ntohs(client_address.sin_port) << endl;
+        if (sockfd != -1)
+            close(sockfd);
+        if (isock != -1)
+            close(isock);
 
-        // Handle client responses in a separate thread
-        thread clientResponseThread(handleClientResponse, client_fd);
-        clientResponseThread.detach();
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        {
+            cerr << strerror(errno) << "\n";
+            isError = true;
+            loops++;
+            continue;
+        }
+        printf("Socket created\n");
 
-        // Handle sending messages to the client in the main thread
-        sendMessageToClient(client_fd);
+        memset(&saddr, '\0', sizeof(saddr));
+        saddr.sin_family = AF_INET;
+        saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        saddr.sin_port = htons(port);
 
-        // Close client socket after communication is done
-        close(client_fd);
+        if ((bind(sockfd, (struct sockaddr *)&saddr, sizeof(saddr)) < 0))
+        {
+            cerr << strerror(errno) << "\n";
+            isError = true;
+            loops++;
+            continue;
+        }
+        printf("Socket bound\n");
+
+        if (listen(sockfd, 5) < 0)
+        {
+            cerr << strerror(errno) << "\n";
+            isError = true;
+            loops++;
+            continue;
+        }
+        printf("Socket listening\n");
+
+        clen = sizeof(caddr);
+        if ((isock = accept(sockfd, (struct sockaddr *)&caddr, &clen)) < 0)
+        {
+            cerr << strerror(errno) << "\n";
+            isError = true;
+            loops++;
+            continue;
+        }
+        printf("Client connection accepted\n");
+
+    } while (isError);
+
+    thread t1(socket_Receive, isock);
+
+    string message;
+    while (true)
+    {
+        cout << "Enter Message, or 'e' to exit\n";
+        getline(cin, message);
+        if (message == "e")
+            break;
+
+        socket_Send(isock, 3, message);
     }
 
-    close(server_fd);
+    close(sockfd);
+    cout << "Socket closed\n";
+    exitThread.store(true);
 
-    return 0;
+    t1.join();
+    cout << "Thread(s) joined\n";
 }
